bdec4be4c091e5ba75d76ef5d7ec6df047da21f2
Jordan Lei <thunderflash.lei@gmail.com>
2020-01-05 03:37:34 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
-import Landing from './components/landing.js';
 import Dev from './components/dev.js';
 import ArtHome from './art_components/art-home.js';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import BlogHome from './blog_components/blog-home.js';
+import {HashRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 import './css/mobile.css';
 
@@ -11,9 +11,9 @@ function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
       <div>
-        <Route path="/" exact component={Landing} />
-        <Route path="/dev" component = {Dev} />
+        <Route path="/" exact component={Dev} />
         <Route path="/art" component = {ArtHome} />
+        <Route path="/blog" component = {BlogHome} />
       </div>
     </Router>
   );
