9742d6493736770fa1d516c33d67ef21be218fb4
Jordan Lei <thunderflash.lei@gmail.com>
2026-04-26 20:10:09 -0400

    fixing some of the shadows

--- components/education.js
+++ components/education.js
@@ -5,8 +5,8 @@ const items = [
     image: "/images/experience/logo-nyu.jpeg",
     caption: (
       <>
-        <h4>PhD Candidate in Neuroscience</h4>
-        <h3>New York University</h3>
+        <h4>New York University</h4>
+        <h3>PhD in Neuroscience</h3>
         <p>
           In Progress <br />
           <b>Thesis</b> | Neural and Cognitive Mechanisms of Planning
@@ -22,8 +22,8 @@ const items = [
     image: "/images/experience/logo-penn.png",
     caption: (
       <>
-        <h4>MSE in Computer Science</h4>
-        <h3>University of Pennsylvania</h3>
+        <h4>University of Pennsylvania</h4>
+        <h3>MSE in Computer Science</h3>
         <p>
           Class of 2021, Summa Cum Laude <br />
           <b>Thesis</b> | Object-based Attention Through Internal Gating
@@ -39,11 +39,10 @@ const items = [
     image: "/images/experience/logo-penn.png",
     caption: (
       <>
-        <h4>
-          BS in Economics, Operations, Info, &amp; Decisions <br />
-          BS in Engineering, Computer Science
-        </h4>
-        <h3>University of Pennsylvania</h3>
+        <h4>University of Pennsylvania</h4>
+        <h3>
+          BS in Economics &amp; BS in Engineering (Computer Science)
+        </h3>
         <p>
           Class of 2020, Summa Cum Laude <br />
           Jerome Fisher Program of Management &amp; Technology <br />
